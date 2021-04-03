b6eb943a911d50e96b913573ef5bdbacb4f743e7
SajidLakhani <sajidlakhani59@gmail.com>
2021-04-03 15:06:51 +0500

    Landing page completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './global.scss';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import KeyToCreating from './components/KeyToCreating/KeyToCreating';
+import RelatedContentCard from './components/RelatedContentCard/RelatedContentCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className = "for-mobile">
+      Mobile Version Coming Soon 
     </div>
+     <div className="App">
+      <Header/>
+      <KeyToCreating/>
+      <RelatedContentCard/>
+      <Footer/>
+    </div>
+    </>
+   
   );
 }
 
